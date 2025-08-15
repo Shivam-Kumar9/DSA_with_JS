aab52ca44b07e2e120fbf14beec46492ce0fbdfd
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-08-15 23:54:48 +0530

    dsa commit

---
+++ sheryans_dsa/12_binary_Search/3.js
@@ -0,0 +1,23 @@
+// BBinary search
+//  problem
+
+//  Given a sorted array and a target value, return the index of the target value in the array.
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1; // Target not found
+}
