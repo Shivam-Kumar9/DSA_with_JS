0b2fa7c38c2759eb3e12756d645c8fcd81846fc5
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-08-19 23:48:01 +0530

    Quick short basic

---
+++ sheryans_dsa/14_QuickSort/1.js
@@ -0,0 +1,13 @@
+//what is quick sort?
+
+// How does Quick Sort work?
+// real world applications of Quick Sort
+/* criteria                         Quick                 Merge
+best timeComplex                O(n log n)               O(n log n)
+Average  tC                O(n log n)                   O(n log n)
+worst    TC        O(n^2) if pivot is poor chosen      O(n log n) (consistent) 
+stability                         not stable           stable 
+
+*/
+// pivot & partition 
+// this also divide and conquror algo
