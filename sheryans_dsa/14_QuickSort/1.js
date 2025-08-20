86dd6a7d41cbdc1921e4554591a044c862956551
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-08-20 23:48:25 +0530

    quick short - just

--- sheryans_dsa/14_QuickSort/1.js
+++ sheryans_dsa/14_QuickSort/1.js
@@ -9,5 +9,19 @@ worst    TC        O(n^2) if pivot is poor chosen      O(n log n) (consistent)
 stability                         not stable           stable 
 
 */
-// pivot & partition 
+// pivot & partition
 // this also divide and conquror algo
+
+function findPivotTdx(arr, low, high) {
+    let pivot = arr[high];
+    let i = low - 1;
+
+    for (let j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+    return i + 1;
+}
