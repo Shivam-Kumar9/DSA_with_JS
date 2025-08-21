b5941037ac0cc55403e9e50d5fdec892637378b5
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-08-21 20:32:48 +0530

    Quick short recursive and  time complexity

--- sheryans_dsa/14_QuickSort/1.js
+++ sheryans_dsa/14_QuickSort/1.js
@@ -12,16 +12,43 @@ stability                         not stable           stable
 // pivot & partition
 // this also divide and conquror algo
 
-function findPivotTdx(arr, low, high) {
-    let pivot = arr[high];
-    let i = low - 1;
 
-    for (let j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            [arr[i], arr[j]] = [arr[j], arr[i]];
+ // merge - need space but it is fast ----------------------
+ // quick - no need of space but it is slow------------------
+
+ // inside while 
+
+
+function findPivotTdx(arr, first, last) { // O(n) tc
+    let pivot = arr[first]; 
+    let i = first + 1 , j = last;
+    while(i<=j){
+        while(i<=last && arr[i]<=pivot) i++;  // these 2 not increasing time complexity done one by one
+        while(j>=first && arr[j]>pivot) j--;  // and in side no operation to doo
+
+        if(i<j){
+            swap(arr, i, j);
         }
     }
-    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
-    return i + 1;
+    swap(arr,first,j)
+    return j;
 }
+function  quickSort(arr, first , last){ // log(n)
+    if(first>=last) return;
+    let pIdx = findPivotIdx(arr, first , last)
+    quickSort(arr, first, pIdx-1)
+    quickSort(arr, pIdx+1, last)
+} // worst case n^2 when all are accending or decending
+//it create pivit in each step  backside  (n-1) , (n-2), (n-3) 
+//so natural num ap series  (n-1) + (n-2) + (n-3) = n(n-1)/2
+// n^2
+
+function swap(arr, i, j){
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+};
+
+let arr = [5, 3, 8, 100, 4, 2, 9, 10, 12];
+quickSort(arr, 0, arr.length - 1);
+console.log(arr);
