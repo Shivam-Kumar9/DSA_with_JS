3b096c77b5b3f087cf856663b55f52dd2f8b627f
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-08-21 20:45:55 +0530

    cyclic sorting  basic

---
+++ sheryans_dsa/15_cyclicShort/1.js
@@ -0,0 +1,15 @@
+let arr = [4, 7, 9, 3, 6, 2, 1, 5, 8]
+
+
+let i = 0;
+
+while(i<arr.length){
+    let correctIndex = arr[i]-1; // since elements are from 1 to n, we can use value as index
+    if(arr[i] != arr[correctIndex] ) {
+        // swap
+        [arr[i], arr[correctIndex]] = [arr[correctIndex], arr[i]];
+    } else {
+        i++;
+    }
+}
+console.log(arr); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
