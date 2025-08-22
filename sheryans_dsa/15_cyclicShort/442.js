20eae060e07a2a72418170a959749a97d5dd042b
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-08-23 03:44:50 +0530

    leet code 442 cyclic sorting

--- sheryans_dsa/15_cyclicShort/442.js
+++ sheryans_dsa/15_cyclicShort/442.js
@@ -5,8 +5,20 @@ let ar = [4, 3, 2, 7, 8, 2, 3, 1];
 let i=0;
  
 while(i<ar.length){
-    let rightIndex = i+1
-    if(ar[i] != ar[rightIndex] && rightIndex < ar.length ){
-        
+    let rightIndex = ar[i]-1;
+    if(ar[i] != ar[rightIndex] ){
+        let temp = ar[rightIndex]
+        ar[rightIndex] = ar[i]
+        ar[i] = temp
     }
+    else i++
 }
+
+let ep = []
+
+for(let i=0; i<ar.length; i++){
+    if(ar[i] != i+1) ep.push(ar[i])
+}
+
+console.log(ar)
+console.log(ep);
