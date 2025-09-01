7f2d3961cd064bbcc673959a3729a483eac39803
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-09-01 23:32:47 +0530

    Add while loop to print countdown from 10; update print statement for fruit lengths

--- dsa_python/basic/cond.py
+++ dsa_python/basic/cond.py
@@ -21,4 +21,11 @@ print()
 # Fruits
 fruits = ['apple', 'banana', 'cherry']
 fruit_lengths = {fruit: len(fruit) for fruit in fruits}
-print(fruit_lengths)
+print(fruit_lengths) #{'apple': 5, 'banana': 6, 'cherry': 6}
+
+#write me a while loop which prints
+# 1. 10, 9
+i = 10
+while i > 0:
+    print(i, end=", " if i > 1 else "\n")
+    i -= 1
