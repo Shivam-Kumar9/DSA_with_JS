d5c37927e3a33252a241d02797423a17d2c61f17
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-09-02 23:46:25 +0530

    Refactor cond.py to comment out unused code and improve readability; add functionality to reverse and print the array

--- dsa_python/basic/cond.py
+++ dsa_python/basic/cond.py
@@ -1,31 +1,37 @@
 # list comprehension
 ar = [1,2,3,4,5]
 
-a = []
-ln = len(ar)-1
-for i in ar:
-    a.append(i**2)
-square = [num**2 for num in ar] # list comprehend
-print(square)
-print( a)
+# a = []
+# ln = len(ar)-1
+# for i in ar:
+#     a.append(i**2)
+# square = [num**2 for num in ar] # list comprehend
+# print(square)
+# print( a)
 
-# index , value for each element in the list 
-fr = [ 'apple', 'banana', 'cherry']
+# # index , value for each element in the list 
+# fr = [ 'apple', 'banana', 'cherry']
  
-for index, fruit in enumerate(fr):
-    print(index, fruit)
-print()
-print()
+# for index, fruit in enumerate(fr):
+#     print(index, fruit)
+# print()
+# print()
 
 
-# Fruits
-fruits = ['apple', 'banana', 'cherry']
-fruit_lengths = {fruit: len(fruit) for fruit in fruits}
-print(fruit_lengths) #{'apple': 5, 'banana': 6, 'cherry': 6}
+# # Fruits
+# fruits = ['apple', 'banana', 'cherry']
+# fruit_lengths = {fruit: len(fruit) for fruit in fruits}
+# print(fruit_lengths) #{'apple': 5, 'banana': 6, 'cherry': 6}
 
-#write me a while loop which prints
-# 1. 10, 9
-i = 10
-while i > 0:
-    print(i, end=", " if i > 1 else "\n")
-    i -= 1
+# #write me a while loop which prints
+# # 1. 10, 9
+# i = 10
+# while i > 0:
+#     print(i, end=", " if i > 1 else "\n")
+#     i -= 1
+
+
+# Q3. 
+
+news =  ar[::-1]
+print(news)
