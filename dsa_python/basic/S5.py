df078552c1a3fd741433662760ad55cc6c6ee0ce
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-09-26 23:59:41 +0530

    Refactor S5.py to comment out unused print statements and improve code clarity

--- dsa_python/basic/S5.py
+++ dsa_python/basic/S5.py
@@ -2,6 +2,19 @@
 
 myTuple = (1,3,4,6,5,8,)
 leng = len(myTuple)-1
-print(le)
+# print(le)
 # print(myTuple[::-1])
-print(myTuple[leng-1:0:-1])
+# print(myTuple[leng-1:0:-1])
+
+
+#tuple packing & un packing
+
+#pack 
+pack = 1,2,3
+# print(pack)
+
+# unpack
+a, b, c = pack
+# print(a)
+# print(b)
+# print(c)
