a5a490f49aa12639f4afbacf1df0472ab5f2e964
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2025-10-23 22:08:10 +0530

    Add JavaScript implementation for counting occurrences of three consecutive numbers summing to a target in a 2D array; update sample input in Python file and add PSC questions to q.txt

---
+++ dsa_python/basic/PSC1/1.js
@@ -0,0 +1,12 @@
+
+
+function findSumCount(input, s) {
+    let count = 0;
+
+    
+}
+
+    
+
+
+findSumCount([[3 ,2 ,1], [2 ,2 ,2], [1 ,5 ,1]], 6)
--- dsa_python/basic/PSC1/1.py
+++ dsa_python/basic/PSC1/1.py
@@ -9,50 +9,56 @@ You need to find occurences of the sum of  three conscutive numbers (i.e x,y and
 '''
 # Sample Input 1
 '''
-1 2 3
-4 5 6
-7 8 9
+3 3 6
+
+3 2 1
+2 2 2
+1 5 1
 '''
 # Sample Output 1
 '''
 2
 '''
 #solve  the problem
-def count_occurrences(matrix, s):
-    n = len(matrix)
-    m = len(matrix[0]) if n > 0 else 0
-    count = 0
 
-    # Check horizontally
-    for i in range(n):
-        for j in range(m - 2):
-            if matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2] == s:
-                count += 1
 
-    # Check vertically
-    for i in range(n - 2):
-        for j in range(m):
-            if matrix[i][j] + matrix[i + 1][j] + matrix[i + 2][j] == s:
-                count += 1
 
-    # Check diagonally (top-left to bottom-right)
-    for i in range(n - 2):
-        for j in range(m - 2):
-            if matrix[i][j] + matrix[i + 1][j + 1] + matrix[i + 2][j + 2] == s:
-                count += 1
 
-    # Check diagonally (top-right to bottom-left)
-    for i in range(n - 2):
-        for j in range(2, m):
-            if matrix[i][j] + matrix[i + 1][j - 1] + matrix[i + 2][j - 2] == s:
-                count += 1
+# def count_occurrences(matrix, s):
+#     n = len(matrix)
+#     m = len(matrix[0]) if n > 0 else 0
+#     count = 0
 
-    return count   
-# Example usage
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-s = 15
-print(count_occurrences(matrix, s)) 
+#     # Check horizontally
+#     for i in range(n):
+#         for j in range(m - 2):
+#             if matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2] == s:
+#                 count += 1
+
+#     # Check vertically
+#     for i in range(n - 2):
+#         for j in range(m):
+#             if matrix[i][j] + matrix[i + 1][j] + matrix[i + 2][j] == s:
+#                 count += 1
+
+#     # Check diagonally (top-left to bottom-right)
+#     for i in range(n - 2):
+#         for j in range(m - 2):
+#             if matrix[i][j] + matrix[i + 1][j + 1] + matrix[i + 2][j + 2] == s:
+#                 count += 1
+
+#     # Check diagonally (top-right to bottom-left)
+#     for i in range(n - 2):
+#         for j in range(2, m):
+#             if matrix[i][j] + matrix[i + 1][j - 1] + matrix[i + 2][j - 2] == s:
+#                 count += 1
+
+#     return count   
+# # Example usage
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# s = 15
+# print(count_occurrences(matrix, s)) 
